d7469a94bf4ce895163ce770679c944bccc5c7be
HO HUYNH MINH HIEU <hohuynhinhhieu@gmail.com>
2017-10-23 12:44:16 +0700

    reformat code

--- app/src/main/java/com/hhmhieu/vnuklmslogin/Deadline.java
+++ app/src/main/java/com/hhmhieu/vnuklmslogin/Deadline.java
@@ -1,5 +1,9 @@
 package com.hhmhieu.vnuklmslogin;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Tran Viet Thanh on 10/22/2017.
  */
